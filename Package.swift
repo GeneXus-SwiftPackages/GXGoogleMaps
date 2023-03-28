d286070d92d863b87d1f3b073c126bfbd1f3d1b9
genexusbot <builder@genexus.com>
2023-03-28 14:18:41 -0300

    Package version: 1.0.0-beta+20230328171001

--- Package.swift
+++ Package.swift
@@ -25,8 +25,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXGoogleMaps",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXGoogleMaps-1.0.0-beta+20230328162416.xcframework.zip",
-			checksum: "edae50e932148c62461fc56e44cbbb21f797e1de95f2ab0005efd1756f440420"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXGoogleMaps-1.0.0-beta+20230328171001.xcframework.zip",
+			checksum: "601be7ca0c2326bfdf149d96e7ac99cf7b122cce68bd67c2b62096212b809592"
 		)
 	]
 )
