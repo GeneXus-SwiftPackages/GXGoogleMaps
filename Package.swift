3693fa774658b64ebf06dba27edeff75bfb938e7
genexusbot <builder@genexus.com>
2023-07-29 18:46:07 -0300

    Package version: 1.1.0-rc.1

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXGoogleMaps",
+	platforms: [.iOS("12.0")],
+	products: [
+		.library(
+			name: "GXGoogleMaps",
+			targets: ["GXGoogleMapsWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreModule_Common_Maps.git", exact: "1.1.0-rc.1"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0-rc.1"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXUCMaps.git", exact: "1.1.0-rc.1")
+	],
+	targets: [
+		.target(name: "GXGoogleMapsWrapper",
+				dependencies: [
+					"GXGoogleMaps",
+					.product(name: "GXCoreModule_Common_Maps", package: "GXCoreModule_Common_Maps", condition: .when(platforms: [.iOS])),
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS])),
+					.product(name: "GXUCMaps", package: "GXUCMaps", condition: .when(platforms: [.iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXGoogleMaps",
+			url: "https://pkgs.genexus.dev/iOS/releases/GXGoogleMaps-1.1.0-rc.1.xcframework.zip",
+			checksum: "c36df68a5bc2ed065e2fffd428a985793d30d4d873e464611404a1d13611c569"
+		)
+	]
+)
