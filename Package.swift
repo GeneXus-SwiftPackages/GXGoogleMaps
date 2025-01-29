12917e4d2c31c8fd9eae9db3ec0d46aebe13a681
The iOS Team @ GeneXus <builder@genexus.com>
2025-01-29 18:35:54 -0300

    Package version: 3.0.0-beta.14

--- Package.swift
+++ Package.swift
@@ -10,11 +10,9 @@ let package = Package(
 			targets: ["GXGoogleMapsWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreModule_Common_Maps.git", exact: "3.0.0-beta.13"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "3.0.0-beta.13"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXUCMaps.git", exact: "3.0.0-beta.13"),
-		.package(url: "https://github.com/googlemaps/ios-maps-sdk", .upToNextMajor(from: "9.0.0")),
-		.package(url: "https://github.com/googlemaps/google-maps-ios-utils", .upToNextMajor(from: "6.0.0"))
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreModule_Common_Maps.git", exact: "3.0.0-beta.14"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "3.0.0-beta.14"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXUCMaps.git", exact: "3.0.0-beta.14")
 	],
 	targets: [
 		.target(name: "GXGoogleMapsWrapper",
@@ -22,15 +20,13 @@ let package = Package(
 					"GXGoogleMaps",
 					.product(name: "GXCoreModule_Common_Maps", package: "GXCoreModule_Common_Maps", condition: .when(platforms: [.iOS])),
 					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS])),
-					.product(name: "GXUCMaps", package: "GXUCMaps", condition: .when(platforms: [.iOS])),
-					.product(name: "GoogleMaps", package: "ios-maps-sdk", condition: .when(platforms: [.iOS])),
-					.product(name: "GoogleMapsUtils", package: "google-maps-ios-utils", condition: .when(platforms: [.iOS]))
+					.product(name: "GXUCMaps", package: "GXUCMaps", condition: .when(platforms: [.iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXGoogleMaps",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXGoogleMaps-3.0.0-beta.13.xcframework.zip",
-			checksum: "239df86eccd2488d9b1c8598ad05b8aac01e0ac65f87fd7f909549f443f18fc1"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXGoogleMaps-3.0.0-beta.14.xcframework.zip",
+			checksum: "a62aa8db72698936984f8232392d991688c53518e86f8f7ab071aeb903914409"
 		)
 	]
 )
