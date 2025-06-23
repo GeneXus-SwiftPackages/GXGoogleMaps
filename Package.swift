cf01a8486f6eed9308c7f57dcbebb4a338103eb3
Fabian Inthamoussu <f.inthamoussu@globant.com>
2025-06-23 17:48:52 +0200

    Update local GX packages dependencies

--- Package.swift
+++ Package.swift
@@ -12,44 +12,36 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/googlemaps/ios-maps-sdk.git", .upToNextMajor(from: "9.0.0")),
-		.package(url: "https://github.com/googlemaps/google-maps-ios-utils.git", .upToNextMajor(from: "6.0.0"))
-	] + Package.Dependency.gxFrameworks(remotePackageUrls: [
-		"https://github.com/GeneXus-SwiftPackages/GXCoreUI.git",
-		"https://github.com/GeneXus-SwiftPackages/GXCoreModule_Common_Maps.git",
-		"https://github.com/GeneXus-SwiftPackages/GXUCMaps.git",
-	]),
+		.package(url: "https://github.com/googlemaps/google-maps-ios-utils.git", .upToNextMajor(from: "6.0.0")),
+		.gxFramework("GXCoreUI"),
+		.gxFramework("GXCoreModule_Common_Maps"),
+		.gxFramework("GXUCMaps"),
+	],
 	targets: [
 		.target(name: "GXGoogleMaps",
 				dependencies: [
 					.product(name: "GoogleMaps", package: "ios-maps-sdk"),
 					.product(name: "GoogleMapsUtils", package: "google-maps-ios-utils"),
-					.gxFrameworkProduct(name: "GXCoreUI", remotePackage: "GXCoreUI"),
-					.gxFrameworkProduct(name: "GXCoreModule_Common_Maps", remotePackage: "GXCoreModule_Common_Maps"),
-					.gxFrameworkProduct(name: "GXUCMaps", remotePackage: "GXUCMaps"),
+					.product(name: "GXCoreUI", package: "GXCoreUI"),
+					.product(name: "GXCoreModule_Common_Maps", package: "GXCoreModule_Common_Maps"),
+					.product(name: "GXUCMaps", package: "GXUCMaps"),
 				]),
 	]
 )
 
 extension Package.Dependency {
-	static func gxFrameworks(remotePackageUrls: [String]) -> [Package.Dependency] {
-		if let localPath = gxFrameworksLocalPath() {
-			return [ .package(name: "GXFrameworksLocal", path: localPath) ]
+	static func gxFramework(_ name: String) -> Package.Dependency {
+		guard let localPath = gxFrameworksLocalPath() else {
+			return .package(url: "https://github.com/GeneXus-SwiftPackages/\(name).git", .upToNextMajor(from: GX_FC_LAST_VERSION))
 		}
-		return remotePackageUrls.map {
-			.package(url: $0, .upToNextMajor(from: GX_FC_LAST_VERSION))
-		}
+		return .package(name: name, path: localPath.appending(component: name).path)
 	}
 }
 
-extension Target.Dependency {
-	static func gxFrameworkProduct(name: String, remotePackage: String) -> Self {
-		.product(name: name, package: gxFrameworksLocalPath() != nil ? "GXFrameworksLocal" : remotePackage)
-	}
-}
-
-func gxFrameworksLocalPath() -> String? {
+func gxFrameworksLocalPath() -> URL? {
 	guard let pathCString = getenv("GX_FRAMEWORKS_LOCAL_PATH") else {
 		return nil
 	}
-	return String(cString: pathCString)
+	let path = String(cString: pathCString)
+	return URL(filePath: path)
 }
