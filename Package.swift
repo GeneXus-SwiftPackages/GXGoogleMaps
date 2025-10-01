d626fa6092101a9e86d15df7dde297057bd4f0dc
Fabian Inthamoussu <f.inthamoussu@globant.com>
2025-10-01 13:33:54 +0200

    Update google maps dependencies to latest version (10.x) - New minimum deployment target for iOS is 16.0.

--- Package.swift
+++ Package.swift
@@ -6,12 +6,12 @@ let GX_FC_LAST_VERSION = Version("3.2.0-beta")
 
 let package = Package(
 	name: "GXGoogleMaps",
-	platforms: [.iOS(.v15), .tvOS("18.0"), .watchOS(.v10), .visionOS("2.0")],
+	platforms: [.iOS(.v16), .tvOS("18.0"), .watchOS(.v10), .visionOS("2.0")],
 	products: [
 		.library(name: "GXGoogleMaps", targets: ["GXGoogleMaps"]),
 	],
 	dependencies: [
-		.package(url: "https://github.com/googlemaps/ios-maps-sdk.git", .upToNextMajor(from: "9.0.0")),
+		.package(url: "https://github.com/googlemaps/ios-maps-sdk.git", .upToNextMajor(from: "10.0.0")),
 		.package(url: "https://github.com/googlemaps/google-maps-ios-utils.git", .upToNextMajor(from: "6.0.0")),
 		.gxFramework("GXCoreUI"),
 		.gxFramework("GXCoreModule_Common_Maps"),
